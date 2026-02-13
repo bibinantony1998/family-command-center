dcaccda18f8894fc32a750e67cd2a14a7fc52a44
Bibin Antony <bibinantony1998@gmail.com>
2026-02-13 22:13:28 +0530

    version 1.19

--- mobile-app/src/screens/Menu.tsx
+++ mobile-app/src/screens/Menu.tsx
@@ -63,7 +63,7 @@ export default function MenuScreen() {
                     <Text style={styles.signOutText}>Sign Out</Text>
                 </TouchableOpacity>
 
-                <Text style={styles.version}>Version 1.18</Text>
+                <Text style={styles.version}>Version 1.19</Text>
             </ScrollView>
         </View>
     );
